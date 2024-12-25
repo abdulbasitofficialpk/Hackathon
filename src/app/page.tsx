917d388debcf6f1606dc5b2c1b9e2d452bb9d0f8
abdulbasitofficialpk <abdulbasit21s21@gmail.com>
2024-12-26 00:20:30 +0500

    ecommerce

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+// import Image from "next/image";
+import Footer from './components/footer';
+import Navbar from './components/Navbar'
+
+
+export default function Home() {
+  return (
+   <div>
+    <Navbar />
+
+    <Footer />
+
+
+   </div>
+  );
+}
